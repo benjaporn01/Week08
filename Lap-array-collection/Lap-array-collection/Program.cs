7da2950ee990aacdc9ca9d13add3c581a96af2b8
benjaporn01 <59030179@kmitl.ac.th>
2017-10-15 21:00:28 +0700

    add mont (unfinish)

--- Lap-array-collection/Lap-array-collection/Program.cs
+++ Lap-array-collection/Lap-array-collection/Program.cs
@@ -37,6 +37,6 @@ namespace ConsoleAppArray
             Console.WriteLine("-----------------------------------------");
             Console.WriteLine("GPA. = {0:F2}", (data[0] + data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + data[7] + data[8]) / 8);
             Console.ReadLine();
-        }
+        }  
     }
 }
